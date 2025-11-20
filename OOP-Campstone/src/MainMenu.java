cf6890b7466f4bb6d2df23d49db6194c4ebbb97d
Evandle <kingbendict2005@gmail.com>
2025-11-20 15:07:32 +0800

    Second Commit

--- OOP-Campstone/src/MainMenu.java
+++ OOP-Campstone/src/MainMenu.java
@@ -1,2 +1,21 @@
-public class MainMenu {
+import javax.swing.*;
+
+public class MainMenu extends JFrame {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JPanel ContentPanel;
+    private JPanel BtnPanel;
+
+    public MainMenu(){
+        setContentPane(panel1);
+        setSize(600,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    static void main() {
+        new MainMenu();
+    }
+
 }
